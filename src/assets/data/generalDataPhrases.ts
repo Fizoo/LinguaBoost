0ff4cce1189236719fd2e41fb51e5551530ec2c1
Fizoo <oleg.mykytchuk@gmail.com>
2023-07-04 19:42:24 +0300

    add full data with phrases

--- src/assets/data/generalDataPhrases.ts
+++ src/assets/data/generalDataPhrases.ts
@@ -3,15 +3,65 @@ import {popular} from "./phrases/popular";
 import {getPhrases} from "./phrases/get";
 import {shortGlobalPhrases} from "./phrases/shortGlobalPhrases";
 import {top_Upper_Intermediate_Phrases} from "./phrases/topUpperIntermediatePhrases";
+import {greeting} from "./phrases/greeting";
+import { restaurant } from "./phrases/restourant";
+import {resort} from "./phrases/resort";
+import {hotel} from "./phrases/hotel";
+import {shop} from "./phrases/shop";
+import {park} from "./phrases/park";
+import {office} from "./phrases/office";
+import {interview} from "./phrases/interview";
+
 
 export const objPhrases: TopicPhrases[] = [
   {
-    id: 1,
+    id: 0,
     topic: 'Popular',
     data: popular,
     type:'phrase'
   },
-
+  {
+    id: 1,
+    topic: 'Greeting',
+    data: greeting,
+    type:'phrase'
+  },
+  {
+    id: 2,
+    topic: 'Restaurant',
+    data: restaurant,
+    type:'phrase'
+  },
+  {
+    id: 3,
+    topic: 'Resort',
+    data: resort,
+    type:'phrase'
+  },
+  {
+    id: 4,
+    topic: 'Hotel',
+    data: hotel,
+    type:'phrase'
+  },
+  {
+    id: 5,
+    topic: 'Hotel',
+    data: shop,
+    type:'phrase'
+  },
+  {
+    id: 6,
+    topic: 'Walk',
+    data: park,
+    type:'phrase'
+  },
+  {
+    id: 7,
+    topic: 'Office',
+    data: office,
+    type:'phrase'
+  },
   {
     id: 8,
     topic: 'Get',
@@ -30,6 +80,13 @@ export const objPhrases: TopicPhrases[] = [
     data: top_Upper_Intermediate_Phrases,
     type:'phrase'
   },
+  {
+    id: 11,
+    topic: 'Interview',
+    data: interview,
+    type:'phrase'
+  },
+
 
 
 ]
