abe23b3e65f98daffbc2b6fadcc7397286cf6135
Fizoo <oleg.mykytchuk@gmail.com>
2023-10-21 20:10:04 +0300

    Auto-generated commit [ci skip]

--- assets/data/generalBook.ts
+++
@@ -1,7 +0,0 @@
-import {Book} from "../../app/models/book";
-import {theAdventuresOfTomSawyer} from "./book/The Adventures of Tom Sawyer";
-
-export const books:Book[]=[
-  theAdventuresOfTomSawyer
-]
-
