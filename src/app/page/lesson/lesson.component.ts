41e3ca380c89d4545a2055c88e86687f5336bea8
Fizoo <oleg.mykytchuk@gmail.com>
2024-03-03 10:09:49 +0200

    refactor

--- src/app/page/lesson/lesson.component.ts
+++ src/app/page/lesson/lesson.component.ts
@@ -145,7 +145,7 @@ export class LessonComponent implements OnInit, OnDestroy {
   }
 
   isWin(): void {
-    if (this.updateList.length === 2) {
+    if (this.updateList.length === 20) {
 
       const {countMin, counterScore,detailForWordsProgress} = this.getProgressOfDay()
 
